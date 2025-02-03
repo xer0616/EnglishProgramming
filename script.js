24fdac3d1bcc4348b7834bdf8084efe5cf4fb00e
xer0616 <32808110+xer0616@users.noreply.github.com>
2025-02-03 11:16:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ output.getvalue()
         isDone = true;
         resultDiv.style.outline = "2px solid green";
       } catch (executionError) {
-        const waitTime = 10;
+        const waitTime = 30;
         resultDiv.textContent += `\n\nExecution Error:\n\n${executionError.message}`;
         resultDiv.textContent += `\n\n${new Date().toLocaleString()} - Wait ${waitTime} seconds and will retry...\n`;
         code = `Generate python code only without explanations to fix the exception ${executionError.message} of using generated code ${generatedContent}`
